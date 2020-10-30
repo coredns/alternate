a62be8169232e5454f8053a10745b5776c596fae
Chris O'Haver <cohaver@infoblox.com>
2020-10-30 13:25:29 -0400

    bump
    Signed-off-by: Chris O'Haver <cohaver@infoblox.com>

--- setup.go
+++ setup.go
@@ -7,7 +7,7 @@ import (
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
 
-	"github.com/caddyserver/caddy"
+	"github.com/coredns/caddy"
 	"github.com/miekg/dns"
 )
 
