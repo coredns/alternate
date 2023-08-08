6204d80011e70a518917b84a7d11c1798c616c48
Mike Beaumont <mjboamail@gmail.com>
2023-08-08 11:32:32 -0400

    feat: support CoreDNS v1.11.0 (#27)
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- forward.go
+++ forward.go
@@ -7,6 +7,7 @@ import (
 	"github.com/coredns/caddy"
 	"github.com/coredns/coredns/plugin/forward"
 	"github.com/coredns/coredns/plugin/pkg/parse"
+	"github.com/coredns/coredns/plugin/pkg/proxy"
 	"github.com/coredns/coredns/plugin/pkg/transport"
 )
 
@@ -40,7 +41,7 @@ func initForward(c *caddy.Controller) (*forward.Forward, error) {
 		if trans != transport.DNS {
 			return f, fmt.Errorf("only dns transport allowed")
 		}
-		p := forward.NewProxy(h, trans)
+		p := proxy.NewProxy("alternate", h, trans)
 		p.SetExpire(defaultExpire)
 		f.SetProxy(p)
 	}
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/coredns/alternate
 go 1.14
 
 require (
-	github.com/coredns/caddy v1.1.0
-	github.com/coredns/coredns v1.8.0
-	github.com/miekg/dns v1.1.34
-	golang.org/x/net v0.0.0-20200707034311-ab3426394381
+	github.com/coredns/caddy v1.1.1
+	github.com/coredns/coredns v1.11.0
+	github.com/miekg/dns v1.1.55
+	golang.org/x/net v0.14.0
 )
